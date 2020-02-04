d57e528f09edd95b5627b2bc0a643527755035b8
kebab <tavomama@yahoo.com>
2020-02-04 09:47:32 +0200

    paieska

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Selenium {
     public static void main(String args[]){
@@ -17,6 +19,23 @@ public class Selenium {
         searchField.sendKeys("Baranauskas");
         searchField.sendKeys(Keys.ENTER);
 
+        waitForElementById(browser, "sb_form_go");
+        WebElement result = browser.findElement(By.xpath("//*[@id=\"b_tween\"]/span"));
+        String resultString = result.getText().replace(" Results", "").replace(",", "");
+
+        int resultsk = Integer.parseInt(resultString);
+
+        if(resultsk < 50000) {
+
+            System.out.println("Mieli skaitytojai, dažniau skaitykite Baranausko kūrybą");
+        } else{
+            System.out.println("Jaunimas dar neužmiršo Anykščių šilelio");
+        }
+
         browser.close();
+    }
+    private static void waitForElementById(WebDriver browser, String id) {
+        WebDriverWait webdriverwait2 = new WebDriverWait(browser, SECONDS);
+        webdriverwait2.until(ExpectedConditions.elementToBeClickable(By.id(id)));
     }
 }
